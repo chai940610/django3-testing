0277d197109b54748235604da04a928308a8a45f
CYX <chai940610@4b5d82362e5e>
2021-07-07 01:53:00 +0000

    add settings

--- portfolio/personal_portfolio-project/personal_portfolio/settings.py
+++ portfolio/personal_portfolio-project/personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mr9$7i0c&z2@-r^i4k70lc#=j#9^k6h=yt+#08=s^=y$xd2q&f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chai940610.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
